6311e13237bfd00815000a3c4d66d7f01e684035
AHAHAChahahahahahaah <victory.malkovi@mail.ru>
2025-04-20 19:20:40 +0500

    3 сезон

---
+++ import4/overviewerConfig.js
@@ -0,0 +1,74 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "World"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1745158405",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        },
+        "attribution": null
+    },
+    "tilesets": [
+        {
+            "name": "North",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "north",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "World",
+            "last_rendertime": 1745155331,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                -1,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                0,
+                -1,
+                0
+            ],
+            "north_direction": 0
+        },
+        
+    ]
+};
